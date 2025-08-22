bf3d1fb6f25fd92e8791f143257e0941a2085e8d
shraddha195 <shraddhashikare5@gmail.com>
2025-08-22 15:09:21 +0530

    included main method

---
+++ LabraryManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.LabraryManageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabraryManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabraryManageSystemApplication.class, args);
+	}
+
+}
